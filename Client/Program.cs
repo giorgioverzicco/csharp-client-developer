65d2b96c47f16ad2416481c0e9064f9ea2dcae75
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-23 15:36:36 +0200

    end of bonus

---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+﻿var clientPath = @"..\..\..\..\client_activity.txt";
+var developerPath = @"..\..\..\..\developer_activity.txt";
+
+string[] clientActivities = File.ReadAllLines(clientPath);
+string[] developerActivities = File.ReadAllLines(developerPath);
+
+foreach (var activity in clientActivities)
+{
+    Console.WriteLine($"Watching for: {activity}");
+
+    if (developerActivities.Contains(activity))
+    {
+        Console.WriteLine($"This task was already assigned: {activity}\n");
+        continue;
+    }
+
+    var retry = false;
+
+    do
+    {
+        StreamWriter? developerFile = null;
+        try
+        {
+            Console.WriteLine($"Assigning task: {activity}");
+            developerFile = File.AppendText(developerPath);
+            developerFile.WriteLine(activity);
+            retry = false;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("\n[ERROR] Can't assign the task right now, retrying...");
+            retry = true;
+        }
+        finally
+        {
+            developerFile?.Close();
+        }
+    } while (retry);
+}
---
+++ Developer/Program.cs
@@ -0,0 +1,61 @@
+﻿var path = @"..\..\..\..\developer_activity.txt";
+
+while (true)
+{
+    string[] activities;
+    
+    try
+    {
+        activities = File.ReadAllLines(path);
+    }
+    catch (Exception)
+    {
+        activities = Array.Empty<string>();
+    }
+
+    if (activities.Length == 0)
+    {
+        Console.Clear();
+        Console.WriteLine("No activities, found. Waiting...");
+        Thread.Sleep(1000);
+    }
+    
+    foreach (var activity in activities)
+    {
+        Console.WriteLine($"Task taken in charge: {activity}");
+
+        var retry = false;
+
+        do
+        {
+            try
+            {
+                List<string> updatedActivities = new (File.ReadAllLines(path));
+                var isDone = new Random().NextDouble() > 0.5;
+
+                if (isDone)
+                {
+                    updatedActivities.Remove(activity);
+                    File.WriteAllLines(path, activities.ToArray());
+                    
+                    Console.WriteLine($"Task done: {activity}");
+                }
+                else
+                {
+                    updatedActivities.Remove(activity);
+                    updatedActivities.Add(activity);
+                    File.WriteAllLines(path, activities.ToArray());
+                    
+                    Console.WriteLine($"Task still not done, will do later: {activity}");
+                }
+
+                retry = false;
+            }
+            catch (Exception)
+            {
+                retry = true;
+                Console.WriteLine("\n[ERROR] Impossible to complete the task, retrying...\n");
+            }
+        } while (retry);
+    }
+}
